245cb18407e14df30b6da1552d7bc6ae05d4f52b
Brian Rice <riceb53@gmail.com>
2024-09-03 16:33:00 -0700

    products index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,13 @@
 class ProductsController < ApplicationController
-
+  def index
+    @products = Product.all
+    # render "products/index"
+    render :index
+  end
 end
+
+
+# idea 
+# syntax
+# 
+#MVC
