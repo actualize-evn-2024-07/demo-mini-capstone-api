f026bb6e36c2bc47fe6b3f9723328ac5aa79ed2d
Brian Rice <riceb53@gmail.com>
2024-09-12 16:39:27 -0700

    orders create all from params and normal orders show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,20 @@ class OrdersController < ApplicationController
   def create
     # make a new order
     # 
-    p current_user
+    order = Order.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+    order.save
     render json: {message:" hello"}
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render :show
   end
 end
