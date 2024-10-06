f79e1c7f4065048f585b645ec73cccab6f03f0ae
Brian Rice <riceb53@gmail.com>
2024-10-06 10:06:03 -0700

    write has many belongs to associations, not yet has many through

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,21 @@
 class Product < ApplicationRecord
-#   Name - presence - uniqueness
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: { greater_than: 0}
   validates :description, presence: true
   validates :description, length: { in: 10..500 }
-  
-# Price - presence - numericality and not a negative number or 0
-# Description - length of a minimum of 1 and a maximum of 500 characters
 
   belongs_to :supplier
   has_many :images
-  # def supplier
-    # Supplier.find_by(id: supplier_id)
+  has_many :category_products
+
+  # def categories
+  #   categories = []
+  #   category_products = CategoryProduct.where(product_id: id)
+  #   category_products.each do |cp|
+  #     categories << Category.find_by(id: cp.category_id)
+  #   end
+  #   categories
   # end
 
 
@@ -34,14 +37,6 @@ class Product < ApplicationRecord
     tax + price
   end
 
-  # def friendly_image
-  #   if images.length > 0
-  #     return images[0].url
-  #   else
-  #     return "https://snworksceo.imgix.net/rce/26b94560-9203-4db4-9d67-40310ef65e60.sized-1000x1000.jpeg?w=1000"
-  #   end
-  # end
-
   def images_with_default
     if images.length > 0
       return images
@@ -50,7 +45,5 @@ class Product < ApplicationRecord
         {id: 100, url: "https://media.gq.com/photos/5582f8c409f0bee56440b484/master/pass/entertainment-2011-05-cage-match-nic-cage_300x430.jpg", created_at: Time.now, updated_at: Time.now}
       ]
     end
-    # if product has at least one image, use that
-    # if the product doesn't have any images, deafult to "https://media.gq.com/photos/5582f8c409f0bee56440b484/master/pass/entertainment-2011-05-cage-match-nic-cage_300x430.jpg"
   end
 end
