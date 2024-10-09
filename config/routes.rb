361b7c082d550979458d004ce2bf34bef1e8092c
Brian Rice <riceb53@gmail.com>
2024-10-09 16:41:21 -0700

    carted products create

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   get "/orders" => "orders#index"
   post "/orders" => "orders#create"
   get "/orders/:id" => "orders#show"
+
+  post "/carted_products" => "carted_products#create"
 end
 
 
