94cecf10fba1e826b7b8c763b8d3e0b5bf84997d
Brian Rice <riceb53@gmail.com>
2024-10-19 19:28:43 -0700

    add new cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
-# config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5173'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins "example.com", "localhost:5173", "demo-mini-capstone-frontend-pxxr.onrender.com"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
